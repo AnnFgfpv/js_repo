e4e1a51a6bd6838d6e3d820c4b5082468db1e932
AnnFgfpv <nstv2013@gmail.com>
2025-09-19 19:12:31 +0300

    .

--- automation.js
+++
@@ -1,9 +0,0 @@
-const { chromium, firefox, webkit } = require('playwright');
-
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  console.log('Chromium title:', await page.title());
-  await browser.close();
-})();
